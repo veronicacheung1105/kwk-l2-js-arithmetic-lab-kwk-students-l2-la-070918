114038e8919c2119d96742ffc17541d8bec682d7
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 23:19:00 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+var newID = (1000000000 + oldID);
+
+var ageIsValid = Number.isInteger(16)
+
+var randomNumber = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13, 14, 15, 16, 17, 18, 19, 20)
+
+var randomNumber = Math.min(1,2,3,4,5,6,7,8,9,10,11,12,13, 14, 15, 16, 17, 18, 19, 20)
+Math.floor(Math.random()*10)+1;
+/*
+Number.isInteger(42);
+//=> true
+ 
+Number.isInteger(0.42);
+//=> false
+*/
+var randomInteger = Math.round(randomNumber)
+
+var randomUserID = (randomInteger + 1000000000)
