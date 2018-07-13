5b99b887e87bb7aee228ad7a2cbbcb63934935b1
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 23:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ var ageIsValid = Number.isInteger(currentAge)
 
 var randomNumber = Math.floor(Math.random()*21)+0;
 
+var newID = oldID + 1000000000
+var ageIsValid = Number.isInteger(currentAge)
+var randomNumber = Math.floor(Math.random()*21) + 0
+var randomInteger = Math.round(randomNumber)
+var randomUserID = 1000000000 + randomInteger
+
 Number.isInteger(42);
 //=> true
  
