bd5feb541e4c023175a60a190591493c9f98fa33
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 22:52:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ You should NOT try to redefine any of the above variables.
 
 var newID = (1000000000 + oldID);
 
-var ageIsValid = Math.round(currentAge)
+var ageIsValid = Number.isInteger()
 
 
 Number.isInteger(42);
